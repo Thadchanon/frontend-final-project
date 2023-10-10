e1986deb03922fe27f319b9ccc1e88d9583d0a93
Thadchanon <thadchanonjirarat@gmail.com>
2023-10-10 13:50:15 +0700

    add dto.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Login from './pages/Login'
 
 function App() {
   return (
     <>
       <div className="App">
         <Navbar />
-        <Login />
       </div>
     </>
   )
