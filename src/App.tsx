1dbff2d8a8dd868c0d3ce800b62de3b8c5b013fa
Thadchanon <thadchanonjirarat@gmail.com>
2023-10-15 23:49:03 +0700

    add GuardedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,24 @@ import Login from './pages/Login'
 import ContentDetail from './pages/ContentDetail'
 import CreateContent from './pages/CreateContent'
 import Edit from './pages/Edit'
+import GuardedRoute from './guards/GuardedRoute'
+import { useAuth } from './providers/AuthProvider'
 
 function App() {
+  const { isLoggedIn } = useAuth()
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/content/:id" element={<ContentDetail />} />
-        <Route path="/create" element={<CreateContent />} />
-        <Route path="/edit/:id" element={<Edit />} />
+        <Route element={<GuardedRoute isRouteAccessible={isLoggedIn} redirectRoute="/login" />}>
+          <Route path="/create" element={<CreateContent />} />
+          <Route path="/edit/:id" element={<Edit />} />
+        </Route>
+        <Route element={<GuardedRoute isRouteAccessible={!isLoggedIn} redirectRoute="/" />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
       </Routes>
     </div>
   )
--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -1,12 +1,16 @@
 import classes from './CreateButton.module.css'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../providers/AuthProvider'
 
 const CreateButton = () => {
+  const { isLoggedIn } = useAuth()
   return (
     <div className={classes.container}>
-      <Link to="/create" className={classes.link}>
-        <div className={classes.button}>Create new content</div>
-      </Link>
+      {isLoggedIn && (
+        <Link to="/create" className={classes.link}>
+          <div className={classes.button}>Create new content</div>
+        </Link>
+      )}
     </div>
   )
 }
